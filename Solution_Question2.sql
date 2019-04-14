5cb40df97762e6736bb61a81e15a4f447bb8af35
limhye70 <limhye70@gmail.com>
2019-04-14 23:22:05 +0800

    Add solutions

--- Solution_Question2.sql
+++ Solution_Question2.sql
@@ -11,21 +11,21 @@ the same user. (could be in different Order)
 
 /* Return the answer of the question */
 SELECT 
-	Product_A,
+    Product_A,
     Product_B,
     COUNT(CustomerID) AS Unique_User_Count -- count the number of row of customerID by Product_A & Product_B
 FROM (
 	/*a table with the combination of cross sell and corresponding customerID*/
 	SELECT
-		DISTINCT 
-		t1.CustomerID,
-		t1.ProductID AS Product_A,
-		t2.ProductID AS Product_B
+	    DISTINCT 
+	    t1.CustomerID,
+	    t1.ProductID AS Product_A,
+	    t2.ProductID AS Product_B
 	FROM `moneysmart`.`sampleorders` AS t1
-		INNER JOIN `moneysmart`.`sampleorders` AS t2 ON t1.CustomerID = t2.CustomerID -- 
+	    INNER JOIN `moneysmart`.`sampleorders` AS t2 ON t1.CustomerID = t2.CustomerID -- 
 	/*include unique combinations only*/
-    WHERE t1.ProductID <> t2.ProductID -- include rows only when Product_A <> Product_B
-		and t1.ProductID < t2.ProductID -- prevent from double counting. for example, include (A,B) but exclude (B,A).
+        WHERE t1.ProductID <> t2.ProductID -- include rows only when Product_A <> Product_B
+	    and t1.ProductID < t2.ProductID -- prevent from double counting. for example, include (A,B) but exclude (B,A).
     ) a
 GROUP by product_A,Product_B
 ORDER by Unique_User_Count DESC; -- show data from the top cross sell products
